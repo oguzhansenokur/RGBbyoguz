dd87b000cc50eeeb6ea1a72500eda8512f76d8a9
Oğuzhan Şenokur <44192576+oguzhansenokur@users.noreply.github.com>
2020-10-09 11:50:54 +0300

    Update RGBbyoguz.cpp

--- RGBbyoguz.cpp
+++ RGBbyoguz.cpp
@@ -1,62 +1,64 @@
 #include "RGBbyoguz.h"
-
-RGB::RGB_pinMode(int r,int g,int b){
+int r, g,b;
+int rv,gv,bv;
+char color[];
+RGB::RGB_pinMode(r,g,b){
     pinMode(r,OUTPUT);
         pinMode(g,OUTPUT);
     pinMode(b,OUTPUT);
 
 }
 
-RGB::RGB_write_by_string(char color[])
+RGB::RGB_write_by_string( color[])
 {
 switch(color[])
 {
 case "white":
-analogWrite(RGB.r,255);
-analogWrite(RGB.g,255);
-analogWrite(RGB.b,255);
+analogWrite(r,255);
+analogWrite(g,255);
+analogWrite(b,255);
 
 
 break;
 case "lime":
-analogWrite(RGB.r,0);
-analogWrite(RGB.g,255);
-analogWrite(RGB.b,0);
+analogWrite(r,0);
+analogWrite(g,255);
+analogWrite(b,0);
 
 
 break;
 case "Red":
-analogWrite(RGB.r,255);
-analogWrite(RGB.g,0);
-analogWrite(RGB.b,0);
+analogWrite(r,255);
+analogWrite(g,0);
+analogWrite(b,0);
 
 
 break;
 case "blue":
-analogWrite(RGB.r,0);
-analogWrite(RGB.g,0);
-analogWrite(RGB.b,255);
+analogWrite(r,0);
+analogWrite(g,0);
+analogWrite(b,255);
 
 
 break;
 case "yellow":
-analogWrite(RGB.r,255);
-analogWrite(RGB.g,255);
-analogWrite(RGB.b,0);
+analogWrite(r,255);
+analogWrite(g,255);
+analogWrite(b,0);
 
 
 break;
 case "cyan":
-analogWrite(RGB.r,0);
-analogWrite(RGB.g,255);
-analogWrite(RGB.b,255);
+analogWrite(r,0);
+analogWrite(g,255);
+analogWrite(b,255);
 
 
 break;
 case "magenta":
-analogWrite(RGB.r,255);
-analogWrite(RGB.g,0);
-analogWrite(RGB.b,255);
+analogWrite(r,255);
+analogWrite(g,0);
+analogWrite(b,255);
 
 
 
@@ -78,11 +80,11 @@ break;
 
 
 }
- void RGB::RGB_write_by_values(int rv,int gv,int bv){
+ void RGB::RGB_write_by_values( rv, gv, bv){
 
-analogWrite(RGB.r,rv);
-analogWrite(RGB.g,gv);
-analogWrite(RGB.b,bv);
+analogWrite(r,rv);
+analogWrite(g,gv);
+analogWrite(b,bv);
 
 
  }
