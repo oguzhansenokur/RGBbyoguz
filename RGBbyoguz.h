9e47da1f6180f1b6cc3808df963e7cd0ef4faf75
Oğuzhan Şenokur <44192576+oguzhansenokur@users.noreply.github.com>
2020-10-09 11:55:01 +0300

    Update RGBbyoguz.h

--- RGBbyoguz.h
+++ RGBbyoguz.h
@@ -3,12 +3,12 @@
 #include "Arduino.h"
 
 class RGB{
-        int r,g,b;
-        int color[3];
-        int rv,gv,bv;
+       extern int r,g,b;
+   extern     int color[3];
+     extern   int rv,gv,bv;
         void RGB_pinMode(r,g,b);
         
-        void RGB_write_by_string(char color[]);
+        void RGB_write_by_string( color[]);
 
         void RGB_write_by_values(rv,gv,bv);
 
